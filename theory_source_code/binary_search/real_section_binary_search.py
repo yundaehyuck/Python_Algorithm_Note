9cc43243bf7f99321890ab61f12053e52adc2d5c
yundaehyuck <dbseogur2000@naver.com>
2023-04-02 14:19:41 +0900

    real section binary search

---
+++ theory_source_code/binary_search/real_section_binary_search.py
@@ -0,0 +1,47 @@
+#boj 14609
+from sys import stdin
+
+def binary_search(a,b,n,real,k,c):
+    
+    delta = (b-a)/n
+
+    start = 0
+    end = delta
+
+    while abs(start-end) > 10**(-5):
+        
+        approx = n*(c[-1])
+        
+        mid = start + (end - start)/2
+
+        for j in range(1,k+1):
+            
+            for h in range(n):
+            
+                approx += (c[-(j+1)]*((a+h*delta+mid)**j))
+        
+        approx *= delta
+            
+        if real > approx:
+
+            start = mid + 10**(-5)
+
+        else:
+
+            end = mid
+
+    return end
+        
+k = int(stdin.readline())
+
+c = list(map(int,stdin.readline().split()))
+
+a,b,n = map(int,stdin.readline().split())
+
+real = 0
+
+for i in range(k+1):
+    
+    real += c[i]*(((b**(k+1-i))- (a**(k+1-i)))/(k+1-i))
+
+print(binary_search(a,b,n,real,k,c))
