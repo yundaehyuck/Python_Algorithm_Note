17c07d18bf2ef9716bf8e4bbe75350dd60064159
yundaehyuck <dbseogur2000@naver.com>
2023-09-01 07:36:20 +0900

    fix pick theorem

--- theory_source_code/geometric/pick_theorem+count_lattice_in_border.py
+++ theory_source_code/geometric/pick_theorem+count_lattice_in_border.py
@@ -4,41 +4,77 @@ from sys import stdin
 #A = I+B/2-1 >>> 2A = 2I+B-2
 #2I = 2A+2 - B >>> I = (2A+2-B)//2
 
-#area of triangle
+#area of polygon
 #정수 연산을 위해 1/2을 하지 않는다
 def area(x,y):
+    
+    a = 0
 
-    return abs(x[0]*y[1] - x[1]*y[0])
+    for i in range(m):
+        
+        a += x[i]*y[i+1]
+        a -= y[i]*x[i+1]
+    
+    if a < 0:
+        
+        a = -a
+    
+    return a
 
 #count lattice in edge
 #num of (a,b),(c,d) = gcd(c-a,d-b)+1
 def count_lattice_edge(x,y):
-    
-    a = abs(y[0]-x[0])
-    b = abs(y[1]-x[1])
-    
+
+    #반드시 절댓값을 취해줘야한다.
+    #그렇지 않으면 (1,1),(0,1)의 경우 a = -1, b = 0이고 
+    #gcd = -1이 나오기 때문에, a-1 = -2가 되어 문제 생긴다 
+    a = abs(y[1]-x[1])
+    b = abs(y[0]-x[0])
+
     while b != 0:
         
         a,b = b,a%b
     
-    return a - 1
+    return a-1
 
-while 1:
+T = int(stdin.readline())
 
-    x1,y1,x2,y2,x3,y3 = map(int,stdin.readline().split())
+for t in range(1,T+1):
+    
+    m = int(stdin.readline())
 
-    if x1 == 0 and y1 == 0 and x2 == 0 and y2 == 0 and x3 == 0 and y3 == 0:
+    x = []
+    y = []
 
-        break
+    a = 0
+    b = 0
 
-    else:
-
-        x = [x2-x1,y2-y1]
-        y = [x3-x1,y3-y1]
-
-        a = area(x,y)
-
-        b = count_lattice_edge((x1,y1),(x2,y2)) + count_lattice_edge((x2,y2),(x3,y3)) + count_lattice_edge((x3,y3),(x1,y1)) + 3
+    for _ in range(m):
         
-        #pick's theorem
-        print((a+2 - b)//2)
+        dx,dy = map(int,stdin.readline().split())
+
+        a += dx
+        b += dy
+
+        x.append(a)
+        y.append(b)
+    
+    x.append(x[0])
+    y.append(y[0])
+
+    a = area(x,y)
+
+    e = 0
+
+    for i in range(m):
+
+        e += count_lattice_edge((x[i],y[i]),(x[i+1],y[i+1]))
+        
+    e += m #counting num of verticle in m-polygon
+
+    #pick's theorem 2A = 2I + B - 2(2A = 2I + E - 2)
+    i = a+ 2 - e 
+
+    print(f'Scenario #{t}:')
+    print(i//2,e,a/2)
+    print()
